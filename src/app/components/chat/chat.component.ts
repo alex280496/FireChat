392689f7541ad9a417b9b5b63c6832c8b9930cfb
Alex <omareyes-96@hotmail.com>
2020-05-09 14:10:51 -0500

    mostras los mensajes de firebase

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {ChatService} from '../../providers/chat.service';
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
@@ -7,12 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatComponent implements OnInit {
   mensaje:string='';
-  constructor() { }
+  constructor(public _cs:ChatService) {
+    this._cs.cargarMensajes().subscribe();
+   }
+
+  
 
   ngOnInit() {
   }
   enviar_mensaje(){
     console.log(this.mensaje);
+    if (this.mensaje.length===0){
+      return;
+    }
+    this._cs.agregarMensaje(this.mensaje)
+            .then(()=>{
+              console.log('Mensaje Enviado'); 
+              //la funcion agregarMensaje devuelve una promesa por eso puedo utilizar el then y catch
+            })
+            .catch((err)=>{
+              console.error('error al enviar',err);
+            })
   }
 
 }
